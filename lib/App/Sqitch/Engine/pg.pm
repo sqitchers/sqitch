f03787b34e09919f06f8858ace2f2060e8de49e3
David E. Wheeler <david@justatheory.com>
2012-04-25 18:10:50 -0700

    Stub out the pg and sqlite engines.

---
+++ lib/App/Sqitch/Engine/pg.pm
@@ -0,0 +1,55 @@
+package App::Sqitch::Engine::pg;
+
+use v5.10;
+use strict;
+use warnings;
+use utf8;
+use namespace::autoclean;
+use Moose;
+
+extends 'App::Sqitch::Engine';
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+__END__
+
+=head1 Name
+
+App::Sqitch::Engine::pg - Sqitch PostgreSQL Engine
+
+=head1 Synopsis
+
+  my $pg = App::Sqitch::Engine->load( engine => 'pg' );
+
+=head1 Description
+
+App::Sqitch::Engine::pg provides the PostgreSQL storage engine for Sqitch.
+
+=head1 Author
+
+David E. Wheeler <david@justatheory.com>
+
+=head1 License
+
+Copyright (c) 2012 iovation Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+=cut
---
+++ t/pg.t
@@ -0,0 +1,13 @@
+#!/usr/bin/perl -w
+
+use strict;
+use warnings;
+use v5.10;
+use Test::More tests => 1;
+
+my $CLASS;
+
+BEGIN {
+    $CLASS = 'App::Sqitch::Engine::pg';
+    require_ok $CLASS or die;
+}
