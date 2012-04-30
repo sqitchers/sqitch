7c463bd8fa7588ea220599c3076eea8dd52abec3
David E. Wheeler <david@justatheory.com>
2012-04-30 16:16:09 -0700

    Add Module::Build::Sqitch.
    And have it insall etc files.

---
+++ priv/Module/Build/Sqitch.pm
@@ -0,0 +1,40 @@
+package Module::Build::Sqitch;
+
+use strict;
+use warnings;
+use base 'Module::Build';
+
+sub new {
+    my $self = shift->SUPER::new(@_);
+    $self->add_build_element('etc');
+    return $self;
+}
+
+sub _path_to {
+    my $self = shift;
+    if (my $dir = $self->prefix || $self->install_base) {
+        return File::Spec->catdir($dir, @_);
+    }
+
+    return File::Spec->catdir($Config::Config{prefix}, @_, 'sqitch');
+}
+
+sub process_etc_files {
+    my $self = shift;
+    my $etc = $self->_path_to('etc');
+    $self->install_path(etc => $etc);
+    for my $file (@{ $self->rscan_dir('etc', sub { -f && !/\.\#/ }) }) {
+        $file = $self->localize_file_path($file);
+        # Append .default if file already exists at its ultimate destination.
+        my $dest = -e File::Spec->catfile(
+            $etc,
+            (File::Spec->splitpath($file))[1..-1]
+        ) ? "$file.default" : $file;
+        $self->copy_if_modified(
+            from => $file,
+            to   => File::Spec->catfile($self->blib, $dest)
+        );
+    }
+}
+
+1;
