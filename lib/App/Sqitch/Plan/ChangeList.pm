3f202798ac8780f335d4b7c59b20fe21ccc76132
David E. Wheeler <david@justatheory.com>
2013-01-03 14:54:00 -0800

    Update copyright year.

--- lib/App/Sqitch/Plan/ChangeList.pm
+++ lib/App/Sqitch/Plan/ChangeList.pm
@@ -415,7 +415,7 @@ David E. Wheeler <david@justatheory.com>
 
 =head1 License
 
-Copyright (c) 2012 iovation Inc.
+Copyright (c) 2012-2013 iovation Inc.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
