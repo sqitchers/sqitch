8008666a340f4a399dc0c97cd4ba2602d9c5d33e
David E. Wheeler <david@justatheory.com>
2013-03-25 00:49:58 -0400

    Add Pod to checkout.

--- lib/App/Sqitch/Command/checkout.pm
+++ lib/App/Sqitch/Command/checkout.pm
@@ -151,3 +151,87 @@ sub execute {
 }
 
 1;
+
+__END__
+
+=head1 Name
+
+App::Sqitch::Command::checkout - Revert, change checkout a VCS branch, and redeploy
+
+=head1 Synopsis
+
+  my $cmd = App::Sqitch::Command::checkout->new(%params);
+  $cmd->execute;
+
+=head1 Description
+
+If you want to know how to use the C<checkout> command, you probably want to
+be reading C<sqitch-checkout>. But if you really want to know how the
+C<checkout> command works, read on.
+
+=head1 Interface
+
+=head2 Class Methods
+
+=head3 C<options>
+
+  my @opts = App::Sqitch::Command::checkout->options;
+
+Returns a list of L<Getopt::Long> option specifications for the command-line
+options for the C<checkout> command.
+
+=head2 Instance Methods
+
+=head3 C<execute>
+
+  $checkout->execute;
+
+Executes the checkout command.
+
+=head1 See Also
+
+=over
+
+=item L<sqitch-checkout>
+
+Documentation for the C<checkout> command to the Sqitch command-line client.
+
+=item L<sqitch>
+
+The Sqitch command-line client.
+
+=back
+
+=head1 Authors
+
+=over
+
+=item * Ronan Dunklau <ronan@dunklau.fr>
+
+=item * David E. Wheeler <david@justatheory.com>
+
+=back
+
+=head1 License
+
+Copyright (c) 2013 Ronan Dunklau & iovation Inc.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+=cut
