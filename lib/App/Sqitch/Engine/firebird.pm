05fbdeee242ddd2e9024944b6aac5c307647a8a1
David E. Wheeler <david@justatheory.com>
2013-12-16 12:34:13 -0800

    Require DBD::Firebird v1.15.

--- lib/App/Sqitch/Engine/firebird.pm
+++ lib/App/Sqitch/Engine/firebird.pm
@@ -140,7 +140,7 @@ has tz_offset => (
 
 sub key    { 'firebird' }
 sub name   { 'Firebird' }
-sub driver { 'DBD::Firebird 1.0' }
+sub driver { 'DBD::Firebird 1.15' }
 
 sub _char2ts {
     my $dt = $_[1];
