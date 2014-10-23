61452f42ae97bc356451a7fb04b8fd29f29325e5
David E. Wheeler <david@justatheory.com>
2014-10-23 16:38:29 -0700

    Teach the verify command to use Target.

--- lib/App/Sqitch/Command/verify.pm
+++ lib/App/Sqitch/Command/verify.pm
@@ -83,7 +83,7 @@ sub configure {
 
 sub execute {
     my $self = shift;
-    my %args = $self->parse_args(@_);
+    my %args = $self->parse_args(target => $self->target, args => \@_);
 
     # Die on unknowns.
     if (my @unknown = @{ $args{unknown}} ) {
@@ -96,10 +96,10 @@ sub execute {
     }
 
     # Warn on multiple targets.
-    my $target = $self->target // shift @{ $args{targets} };
+    my $target = shift @{ $args{targets} };
     $self->warn(__x(
         'Too many targets specified; connecting to {target}',
-        target => $target,
+        target => $target->name,
     )) if @{ $args{targets} };
 
     # Warn on too many changes.
@@ -112,7 +112,7 @@ sub execute {
     )) if @{ $args{changes} };
 
     # Now get to work.
-    my $engine = $self->engine_for_target($target);
+    my $engine = $target->engine;
     if (my %v = %{ $self->variables }) { $engine->set_variables(%v) }
     $engine->verify($from, $to);
     return $self;
