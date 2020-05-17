1a41f3255e5053851289fc29ada6df7ee5510e1d
David E. Wheeler <david@justatheory.com>
2020-05-17 11:31:53 -0400

    Update copyright date

--- lib/App/Sqitch/Plan/Line.pm
+++ lib/App/Sqitch/Plan/Line.pm
@@ -347,7 +347,7 @@ David E. Wheeler <david@justatheory.com>
 
 =head1 License
 
-Copyright (c) 2012-2018 iovation Inc.
+Copyright (c) 2012-2020 iovation Inc.
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
