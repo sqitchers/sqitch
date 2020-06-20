00e0dafe8fe8ec451cfa462e5d4d1ca25eb72148
David E. Wheeler <david@justatheory.com>
2020-06-20 13:53:05 -0400

    remove are_deployed_changes from Exasol
    Let the default implementation handle it, since from feedback on issue #521,
    it seems that the partitioning of the IN clause is not required on Exasol.

--- lib/App/Sqitch/Engine/exasol.pm
+++ lib/App/Sqitch/Engine/exasol.pm
@@ -291,23 +291,6 @@ sub is_deployed_tag {
     )->[0];
 }
 
-sub are_deployed_changes {
-    my $self = shift;
-    my @qs;
-    my $i = @_;
-    while ($i > 250) {
-        push @qs => 'change_id IN (' . join(', ' => ('?') x 250) . ')';
-        $i -= 250;
-    }
-    push @qs => 'change_id IN (' . join(', ' => ('?') x $i) . ')' if $i > 0;
-    my $expr = join ' OR ', @qs;
-    @{ $self->dbh->selectcol_arrayref(
-        "SELECT change_id FROM changes WHERE $expr",
-        undef,
-        map { $_->id } @_,
-    ) };
-}
-
 sub _registry_variable {
     my $self   = shift;
     my $schema = $self->registry;
