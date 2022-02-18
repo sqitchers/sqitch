9074a947ce7510a6448edb1056a09c8dd8d36fd3
David E. Wheeler <david@justatheory.com>
2022-02-18 15:44:22 -0500

    Restore installing the MySQL client
    Works around a (temporary) issue with the GitHub instances. See
    actions/virtual-environments#5067 for details.

--- .github/ubuntu/all-apt-prereqs.sh
+++ .github/ubuntu/all-apt-prereqs.sh
@@ -1,11 +1,12 @@
 set -e
 
 sudo apt-get update -qq
+sudo apt-get remove -qq mysql-common # https://github.com/actions/virtual-environments/issues/5067#issuecomment-1038752575
 sudo env DEBIAN_FRONTEND=noninteractive apt-get install -qq \
     libicu-dev gettext aspell-en software-properties-common \
     curl unixodbc-dev odbcinst unixodbc \
     default-jre \
     firebird-dev firebird3.0-utils \
+    mysql-client default-libmysqlclient-dev \
     libarchive-tools
-    # mysql-client default-libmysqlclient-dev \
 cat t/odbc/odbcinst.ini | sudo tee -a /etc/odbcinst.ini
