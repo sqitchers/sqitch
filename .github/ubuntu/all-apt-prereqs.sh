11af907e65ddd460fa4581b87d4c4c748e3f3b83
David E. Wheeler <david@justatheory.com>
2024-12-29 17:36:09 -0500

    Install libaio for Oracle tests, too

--- .github/ubuntu/all-apt-prereqs.sh
+++ .github/ubuntu/all-apt-prereqs.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 set -e
 
 sudo apt-get update -qq
--- .github/ubuntu/oracle.sh
+++ .github/ubuntu/oracle.sh
@@ -5,10 +5,14 @@ set -e
 version=23.6.0.24.10
 icdr=2360000
 
-# Install bsdtar, required to get --strip-components for a zip file.
+# Download dependencies.
 if [ -z "$SKIP_DEPENDS" ]; then
+    # Install libaio and bsdtar, required to get --strip-components for a zip file.
     sudo apt-get update -qq
-    sudo env DEBIAN_FRONTEND=noninteractive apt-get install -qq libarchive-tools
+    sudo env DEBIAN_FRONTEND=noninteractive apt-get install -qq libarchive-tools libaio1t64
+
+    # instantclient still wants libaio.so.1. https://askubuntu.com/a/1514001
+    sudo ln -s /usr/lib/x86_64-linux-gnu/libaio.so.1t64 /usr/lib/x86_64-linux-gnu/libaio.so.1
 fi
 
 # Download Instant Client.
