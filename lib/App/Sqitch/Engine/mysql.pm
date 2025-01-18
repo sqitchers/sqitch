sqitchers/sqitch

commit d17dff97c3c196252d6a99d9ac58b77a7d25ed1e

Switch from DBD::mysql to DBD::MariaDB
DBD::mysql 5 recently dropped support for building from MySQL client
libraries prior to v8. This creates challenges for people still using
earlier versions, as well as those using MariaDB.

DBD::MariaDB forked from DBD::mysql back in 2018, and maintains
compatibility with both MySQL and MariaDB. It remains compatible with
earlier versions of MySQL as well as the MariaDB client libraries. It
also includes bug fixes and more transparent support for Unicode
encoding.

So switch from DBD::mysql to DBD::MariaDB. It's largely a drop-in
replacement, with just a few additional changes:

*   Add `_dsn()` to DBIEngine and always use it to connect to the
    database. Override it in mysql.pm to replace `dbi:mysql` with
    `dbi:MariaDB`. Also override it in some other drivers that mangle
    the value returned by URI::db.
*   Remove incompatible config parameters `mysql_enable_utf8` (no longer
    needed due to the aforementioned Unicode bug fixes),
    `mysql_auto_reconnect`, and `mysql_use_result`.
*   Read DBI handle attributes starting with `mariadb_` instead of
    `mysql_`.
*   Add a `USE` statement to set the registry as the current database
    after it has been created. Not sure how this wasn't required when
    using DBD::mysql, but aligns more closely with the requirements of
    other engines.

Resolves #825.

lib/App/Sqitch/Engine/mysql.pm
@@ -64,6 +64,11 @@ has _mycnf => (
 sub _def_user { $_[0]->_mycnf->{user} || $_[0]->sqitch->sysuser }
 sub _def_pass { $ENV{MYSQL_PWD} || shift->_mycnf->{password} }
 
+sub _dsn {
+    (my $dsn = shift->uri->dbi_dsn) =~ s/\Adbi:mysql/dbi:MariaDB/;
+    return $dsn;
+}
+
 has dbh => (
     is      => 'rw',
     isa     => DBH,
@@ -71,22 +76,18 @@ has dbh => (
     default => sub {
         my $self = shift;
         $self->use_driver;
-        my $uri = $self->registry_uri;
-        my $dbh = DBI->connect($uri->dbi_dsn, $self->username, $self->password, {
-            PrintError           => 0,
-            RaiseError           => 0,
-            AutoCommit           => 1,
-            mysql_enable_utf8    => 1,
-            mysql_auto_reconnect => 0,
-            mysql_use_result     => 0, # Prevent "Commands out of sync" error.
-            HandleError          => $self->error_handler,
-            Callbacks            => {
+        my $dbh = DBI->connect($self->_dsn, $self->username, $self->password, {
+            PrintError  => 0,
+            RaiseError  => 0,
+            AutoCommit  => 1,
+            HandleError => $self->error_handler,
+            Callbacks   => {
                 connected => sub {
                     my $dbh = shift;
                     $dbh->do("SET SESSION $_") or return for (
                         q{character_set_client   = 'utf8'},
                         q{character_set_server   = 'utf8'},
-                        ($dbh->{mysql_serverversion} || 0 < 50500 ? () : (q{default_storage_engine = 'InnoDB'})),
+                        ($dbh->{mariadb_serverversion} || 0 < 50500 ? () : (q{default_storage_engine = 'InnoDB'})),
                         q{time_zone              = '+00:00'},
                         q{group_concat_max_len   = 32768},
                         q{sql_mode = '} . join(',', qw(
@@ -105,7 +106,7 @@ has dbh => (
         });
 
         # Make sure we support this version.
-        my ($dbms, $vnum, $vstr) = $dbh->{mysql_serverinfo} =~ /mariadb/i
+        my ($dbms, $vnum, $vstr) = $dbh->{mariadb_serverinfo} =~ /mariadb/i
             ? ('MariaDB', 50300, '5.3')
             : ('MySQL',   50100, '5.1.0');
         hurl mysql => __x(
@@ -113,7 +114,7 @@ has dbh => (
             rdbms        => $dbms,
             want_version => $vstr,
             have_version => $dbh->selectcol_arrayref('SELECT version()')->[0],
-        ) unless $dbh->{mysql_serverversion} >= $vnum;
+        ) unless $dbh->{mariadb_serverversion} >= $vnum;
 
         return $dbh;
     }
@@ -211,9 +212,9 @@ has _fractional_seconds => (
     lazy    => 1,
     default => sub {
         my $dbh = shift->dbh;
-        return $dbh->{mysql_serverinfo} =~ /mariadb/i
-            ? $dbh->{mysql_serverversion} >= 50305
-            : $dbh->{mysql_serverversion} >= 50604;
+        return $dbh->{mariadb_serverinfo} =~ /mariadb/i
+            ? $dbh->{mariadb_serverversion} >= 50305
+            : $dbh->{mariadb_serverversion} >= 50604;
     },
 );
 
@@ -221,7 +222,7 @@ sub mysql { @{ shift->_mysql } }
 
 sub key    { 'mysql' }
 sub name   { 'MySQL' }
-sub driver { 'DBD::mysql 4.018' }
+sub driver { 'DBD::MariaDB 1.0' }
 sub default_client { 'mysql' }
 
 sub _char2ts {
@@ -282,6 +283,7 @@ sub _initialize {
     # Deploy the registry to the Sqitch database.
     $self->run_upgrade( file(__FILE__)->dir->file('mysql.sql') );
     $self->_set_initialized(1);
+    $self->dbh->do('USE ' . $self->dbh->quote_identifier($self->registry));
     $self->_register_release;
 }
 
@@ -478,7 +480,7 @@ sub _prepare_registry_file {
 
         # Strip out 5.5 stuff on earlier versions.
         $sql =~ s/-- ## BEGIN 5[.]5.+?-- ## END 5[.]5//ms
-            if $self->dbh->{mysql_serverversion} < 50500;
+            if $self->dbh->{mariadb_serverversion} < 50500;
     }
 
     if (!$can_create_checkit) {
@@ -501,11 +503,12 @@ sub _prepare_registry_file {
 # determine whether we have binary logging disabled, or trust function
 # creators, or have the SUPER privilege.
 sub _can_create_immutable_function {
+    # Use md5() to prevent "Illegal mix of collations" errors.
     shift->dbh->selectcol_arrayref(q{
         SELECT @@log_bin = 0
             OR @@log_bin_trust_function_creators = 1
             OR (
-                SELECT super_priv = 'Y' FROM mysql.user
+                SELECT md5(super_priv) = md5('Y') FROM mysql.user
                  WHERE CONCAT(user, '@', host) = current_user
             )
     })->[0]

t/mysql.t
@@ -62,6 +62,7 @@ my $sqitch_uri = $uri->clone;
 $sqitch_uri->dbname('sqitch');
 is $mysql->registry_uri, $sqitch_uri, 'registry_uri should be correct';
 is $mysql->uri, $uri, qq{uri should be "$uri"};
+is $mysql->_dsn, 'dbi:MariaDB:database=mydb', 'DSN should use MariaDB';
 is $mysql->registry_destination, 'db:mysql:sqitch',
     'registry_destination should be the same as registry_uri';
 is $mysql->_lock_name, 'sqitch working on ' . $uri->dbname,
@@ -381,23 +382,23 @@ is $mysql->_limit_default, '18446744073709551615', 'Should have _limit_default';
 
 SECS: {
     my $mock = Test::MockModule->new($CLASS);
-    my $dbh = {mysql_serverinfo => 'foo', mysql_serverversion => 50604};
+    my $dbh = {mariadb_serverinfo => 'foo', mariadb_serverversion => 50604};
     $mock->mock(dbh => $dbh);
     is $mysql->_ts_default, 'utc_timestamp(6)',
         'Should have _ts_default with fractional seconds';
 
-    $dbh->{mysql_serverversion} = 50101;
+    $dbh->{mariadb_serverversion} = 50101;
     my $my51 = $CLASS->new(sqitch => $sqitch, target => $target);
     is $my51->_ts_default, 'utc_timestamp',
         'Should have _ts_default without fractional seconds on 5.1';
 
-    $dbh->{mysql_serverversion} = 50304;
-    $dbh->{mysql_serverinfo} = 'Something about MariaDB man';
+    $dbh->{mariadb_serverversion} = 50304;
+    $dbh->{mariadb_serverinfo} = 'Something about MariaDB man';
     my $maria = $CLASS->new(sqitch => $sqitch, target => $target);
     is $maria->_ts_default, 'utc_timestamp',
         'Should have _ts_default without fractional seconds on early mariadb';
 
-    $dbh->{mysql_serverversion} = 50305;
+    $dbh->{mariadb_serverversion} = 50305;
     is $mysql->_ts_default, 'utc_timestamp(6)',
         'Should have _ts_default with fractional secondson mariadb 5.03.05';
 }
@@ -516,7 +517,7 @@ UPGRADE: {
     my $fracsec;
     my $version = 50500;
     $mock->mock(_fractional_seconds => sub { $fracsec });
-    $mock->mock(dbh => sub { { mysql_serverversion => $version } });
+    $mock->mock(dbh => sub { { mariadb_serverversion => $version } });
     $mock->mock(_can_create_immutable_function => 1);
 
     # Mock run.
@@ -596,12 +597,13 @@ END {
 $uri = URI->new(
     $ENV{SQITCH_TEST_MYSQL_URI} ||
     $ENV{MYSQL_URI} ||
-    'db:mysql://root@/information_schema'
+    'db:mariadb://root@/information_schema'
 );
 $uri->dbname('information_schema') unless $uri->dbname;
 my $err = try {
     $mysql->use_driver;
-    $dbh = DBI->connect($uri->dbi_dsn, $uri->user, $uri->password, {
+    (my $dsn = $uri->dbi_dsn) =~ s/\Adbi:mysql/dbi:MariaDB/;
+    $dbh = DBI->connect($dsn, $uri->user, $uri->password, {
         PrintError  => 0,
         RaiseError  => 0,
         AutoCommit  => 1,
@@ -609,13 +611,13 @@ my $err = try {
     });
 
     # Make sure we have a version we can use.
-    if ($dbh->{mysql_serverinfo} =~ /mariadb/i) {
-        die "MariaDB >= 50300 required; this is $dbh->{mysql_serverversion}\n"
-            unless $dbh->{mysql_serverversion} >= 50300;
+    if ($dbh->{mariadb_serverinfo} =~ /mariadb/i) {
+        die "MariaDB >= 50300 required; this is $dbh->{mariadb_serverversion}\n"
+            unless $dbh->{mariadb_serverversion} >= 50300;
     }
     else {
-        die "MySQL >= 50100 required; this is $dbh->{mysql_serverversion}\n"
-            unless $dbh->{mysql_serverversion} >= 50100;
+        die "MySQL >= 50100 required; this is $dbh->{mariadb_serverversion}\n"
+            unless $dbh->{mariadb_serverversion} >= 50100;
     }
 
     $dbh->do("CREATE DATABASE $db");
@@ -650,7 +652,7 @@ DBIEngineTest->run(
         for my $spec (
             [character_set_client   => qr/^utf8/],
             [character_set_server   => qr/^utf8/],
-            ($dbh->{mysql_serverversion} < 50500 ? () : ([default_storage_engine => qr/^InnoDB$/])),
+            ($dbh->{mariadb_serverversion} < 50500 ? () : ([default_storage_engine => qr/^InnoDB$/])),
             [time_zone              => qr/^\+00:00$/],
             [group_concat_max_len   => qr/^32768$/],
         ) {

.github/ubuntu/mysql.sh
@@ -6,5 +6,5 @@ set -e
 if [ -z "$SKIP_DEPENDS" ]; then
     sudo apt-get update -qq
     sudo apt-get remove -qq mysql-common # https://github.com/actions/virtual-environments/issues/5067#issuecomment-1038752575
-    sudo env DEBIAN_FRONTEND=noninteractive apt-get install -qq mysql-client default-libmysqlclient-dev
+    sudo env DEBIAN_FRONTEND=noninteractive apt-get install -qq mariadb-client libmariadbd-dev
 fi

.github/workflows/coverage.yml
@@ -75,7 +75,7 @@ jobs:
           .github/ubuntu/snowflake.sh
           .github/ubuntu/vertica.sh
       - run: cpm install --verbose --show-build-log-on-failure --no-test --with-recommends --cpanfile dist/cpanfile
-      - run: cpm install --verbose --show-build-log-on-failure --no-test --with-recommends DBI DBD::ODBC DBD::Firebird DBD::Oracle DBD::mysql DBD::Pg Devel::Cover Devel::Cover::Report::Coveralls
+      - run: cpm install --verbose --show-build-log-on-failure --no-test --with-recommends DBI DBD::ODBC DBD::Firebird DBD::Oracle DBD::MariaDB DBD::Pg Devel::Cover Devel::Cover::Report::Coveralls
       - name: Run Tests
         env:
           PERL5LIB: "${{ github.workspace }}/local/lib/perl5"

.github/workflows/mysql.yml
@@ -42,7 +42,7 @@ jobs:
     runs-on: ubuntu-latest
     services:
       # Run the MySQL service in a container we can connect to. Means that the
-      # CLI and libraries DBD::mysql use are static to the version on the runner
+      # CLI and libraries DBD::MariaDB use are static to the version on the runner
       # machine.
       mysql:
         image: "${{ matrix.image }}:${{ matrix.version }}"
@@ -63,7 +63,7 @@ jobs:
           path: local
           key: perl-${{ steps.perl.outputs.perl-hash }}
       - run: cpm install --verbose --show-build-log-on-failure --no-test --with-recommends --cpanfile dist/cpanfile
-      - run: cpm install --verbose --show-build-log-on-failure --no-test --with-recommends DBD::mysql
+      - run: cpm install --verbose --show-build-log-on-failure --no-test --with-recommends DBD::MariaDB
       - name: prove
         env:
           PERL5LIB: "${{ github.workspace }}/local/lib/perl5"

dist.ini
@@ -76,7 +76,7 @@ Test::Spelling = 0
 Test::MockObject::Extends = 1.20180705
 DBD::SQLite = 1.37
 DBD::Pg = 2.0
-DBD::mysql = 4.018
+DBD::MariaDB = 1.0
 MySQL::Config = 0
 DBD::Firebird = 1.11
 DBD::ODBC = 1.59
@@ -107,7 +107,7 @@ DBD::SQLite = 1.37
 [OptionalFeature / mysql]
 -description = Support for managing MySQL databases
 -prompt = 0
-DBD::mysql = 4.018
+DBD::MariaDB = 1.0
 MySQL::Config = 0
 
 [OptionalFeature / firebird]

dist/cpanfile
@@ -71,7 +71,7 @@ suggests "DBD::ODBC" => "1.59";
 suggests "DBD::Oracle" => "1.23";
 suggests "DBD::Pg" => "2.0";
 suggests "DBD::SQLite" => "1.37";
-suggests "DBD::mysql" => "4.018";
+suggests "DBD::MariaDB" => "1.0";
 suggests "MySQL::Config" => "0";
 suggests "Time::HiRes" => "0";
 suggests "Time::Local" => "0";
@@ -120,7 +120,7 @@ on 'develop' => sub {
   requires "DBD::Oracle" => "1.23";
   requires "DBD::Pg" => "2.0";
   requires "DBD::SQLite" => "1.37";
-  requires "DBD::mysql" => "4.018";
+  requires "DBD::MariaDB" => "1.0";
   requires "MySQL::Config" => "0";
   requires "Time::HiRes" => "0";
   requires "Time::Local" => "0";
@@ -131,7 +131,7 @@ on 'develop' => sub {
   recommends "DBD::ODBC" => "1.59";
   recommends "DBD::Pg" => "2.0";
   recommends "DBD::SQLite" => "1.37";
-  recommends "DBD::mysql" => "4.018";
+  recommends "DBD::MariaDB" => "1.0";
   recommends "Dist::Zilla" => "5";
   recommends "Dist::Zilla::Plugin::AutoPrereqs" => "0";
   recommends "Dist::Zilla::Plugin::CPANFile" => "0";

dist/sqitch.spec
@@ -240,7 +240,7 @@ Group:          Development/Libraries
 Requires:       sqitch >= %{version}
 Requires:       mysql >= 5.1.0
 Requires:       perl(DBI) >= 1.631
-Requires:       perl(DBD::mysql) >= 4.018
+Requires:       perl(DBD::MariaDB) >= 1.0
 Requires:       perl(MySQL::Config)
 Provides:       sqitch-mysql
 

commit d2306666cc016ca04d0edca1a48cd976555de136

Restore MySQL connection to the registry
A bug introduced in d17dff9 and shipped in v1.5.0 caused the MySQL
engine to connect to the target database instead of the registry
database. Restore connecting to the registry database. Resolves #862.

lib/App/Sqitch/Engine/mysql.pm
@@ -65,7 +65,7 @@ sub _def_user { $_[0]->_mycnf->{user} || $_[0]->sqitch->sysuser }
 sub _def_pass { $ENV{MYSQL_PWD} || shift->_mycnf->{password} }
 
 sub _dsn {
-    (my $dsn = shift->uri->dbi_dsn) =~ s/\Adbi:mysql/dbi:MariaDB/;
+    (my $dsn = shift->registry_uri->dbi_dsn) =~ s/\Adbi:mysql/dbi:MariaDB/;
     return $dsn;
 }
 

t/mysql.t
@@ -62,7 +62,7 @@ my $sqitch_uri = $uri->clone;
 $sqitch_uri->dbname('sqitch');
 is $mysql->registry_uri, $sqitch_uri, 'registry_uri should be correct';
 is $mysql->uri, $uri, qq{uri should be "$uri"};
-is $mysql->_dsn, 'dbi:MariaDB:database=mydb', 'DSN should use MariaDB';
+is $mysql->_dsn, 'dbi:MariaDB:database=sqitch', 'DSN should use MariaDB';
 is $mysql->registry_destination, 'db:mysql:sqitch',
     'registry_destination should be the same as registry_uri';
 is $mysql->_lock_name, 'sqitch working on ' . $uri->dbname,