sqitchers/sqitch

commit 33291bd2ec9948b0eb866eeeb685a91a44ef67a7

Skip registry creation on Snowflake when necessary
In some cases, the Sqitch user will lack permission to create a schema,
such as when the DBA doesn't want to give it that permission. Implement
`run_upgrade` in the Snowflake engine to detect this lack of permission
by executing `CREATE SCHEMA IF NOT EXISTS` and stripping out `CREATE
SCHEMA` and `COMMENT ON SCHEMA` commands when it fails due to
insufficient privileges. Resolves #828.

lib/App/Sqitch/Engine/snowflake.pm
@@ -213,7 +213,7 @@ has dbh => (
                         "ALTER SESSION SET TIMESTAMP_OUTPUT_FORMAT='YYYY-MM-DD HH24:MI:SS'",
                         "ALTER SESSION SET TIMEZONE='UTC'",
                     );
-                    $dbh->do('USE SCHEMA ' . $self->registry)
+                    $dbh->do('USE SCHEMA ' . $dbh->quote_identifier($self->registry))
                         or $self->_handle_no_registry($dbh);
                     return;
                 },
@@ -299,9 +299,49 @@ sub _initialize {
         schema => $schema
     ) if $self->initialized;
 
-    $self->run_file( file(__FILE__)->dir->file('snowflake.sql') );
+    $self->run_upgrade(file(__FILE__)->dir->file('snowflake.sql') );
     $self->dbh->do("USE SCHEMA $schema");
     $self->_register_release;
+}
+
+# Determines whether we can create a schema and, if so, executes $file.
+# Otherwise creates and executes a copy of $file with schema commands removed.
+sub run_upgrade {
+    my ($self, $file) = @_;
+    try {
+        # Can we create a schema?
+        my $dbh = $self->dbh;
+        my $reg = $dbh->quote_identifier($self->registry);
+        $dbh->do("CREATE SCHEMA IF NOT EXISTS $reg");
+    } catch {
+        die $_ unless $DBI::state && $DBI::state eq '42501'; # ERRCODE_INSUFFICIENT_PRIVILEGE
+        # Cannot create schema; strip out schema stuff.
+        $file = $self->_strip_file($file);
+    };
+
+    # All good.
+    return $self->run_file($file);
+}
+
+# Creates and returns a copy of $file with C<CREATE SCHEMA> and C<COMMENT ON
+# SCHEMA> commands stripped out.
+sub _strip_file {
+    my ($self, $file) = @_;
+    my $in = $file->open('<:raw') or hurl io => __x(
+        'Cannot open {file}: {error}',
+        file  => $file,
+        error => $!
+    );
+
+    require File::Temp;
+    my $out = File::Temp->new;
+    while (<$in>) {
+        s/C(?:REATE|OMMENT ON) SCHEMA\b.+//;
+        print {$out} $_;
+    }
+
+    close $out;
+    return $out;
 }
 
 sub _no_table_error  {

t/snowflake.t
@@ -1,8 +1,8 @@
 #!/usr/bin/perl -w
 
 # To test against a live Snowflake database, you must set the
-# SQITCH_TEST_SNOWFLAKE_URI environment variable. this is a stanard URI::db URI,
-# and should look something like this:
+# SQITCH_TEST_SNOWFLAKE_URI environment variable. this is a standard URI::db
+# URI, and should look something like this:
 #
 #     export SQITCH_TEST_SNOWFLAKE_URI=db:snowflake://username:password@accountname/dbname?Driver=Snowflake;warehouse=warehouse
 #
@@ -15,6 +15,8 @@ use 5.010;
 use Test::More 0.94;
 use Test::MockModule;
 use Test::Exception;
+use Test::File::Contents qw(file_contents_unlike);
+use DBD::Mem;
 use Locale::TextDomain qw(App-Sqitch);
 use Capture::Tiny 0.12 qw(:all);
 use File::Temp 'tempdir';
@@ -514,6 +516,45 @@ is $dt->time_zone->name, 'UTC', 'DateTime TZ should be set';
 ok my $now = App::Sqitch::DateTime->now, 'Construct a datetime object';
 is $snow->_char2ts($now), $now->as_string(format => 'iso'),
     'Should get ISO output from _char2ts';
+
+##############################################################################
+# Test run_upgrade.
+UPGRADE: {
+    my $file;
+    $mock_snow->mock(run_file => sub { $file = $_[1] });
+
+    # Need to mock the database handle.
+    my $dbh = DBI->connect('dbi:Mem:', undef, undef, {});
+    $mock_snow->mock(dbh => $dbh);
+    my $mock_dbh = Test::MockModule->new(ref $dbh, no_auto => 1);
+    my (@do, $do_err);
+    $mock_dbh->mock(do => sub { shift; @do = @_; die $do_err if $do_err });
+
+    # Deploy with schema permissions.
+    my $fn = file($INC{'App/Sqitch/Engine/snowflake.pm'})->dir->file('snowflake.sql');
+    ok $snow->run_upgrade($fn), 'Run upgrade';
+    is $file, $fn, 'Should have executed the unmodified file';
+
+    # Deploy withouit schema permissions.
+    $do_err = 'Ooops';
+    local *DBI::state;
+    $DBI::state = '42501';
+    ok $snow->run_upgrade($fn), 'Run upgrade again';
+    isnt $file, $fn, 'Should have a modified file';
+
+    # Make sure no schema stuff remains in the file.
+    file_contents_unlike $file, qr/CREATE SCHEMA/,
+        'Should have no CREATE SCHEMA';
+    file_contents_unlike $file, qr/COMMENT ON SCHEMA/,
+        'Should have no COMMENT ON SCHEMA';
+
+    # Die with any other error.
+    $DBI::state = '10030';
+    throws_ok { $snow->run_upgrade($fn) } qr/Ooops/,
+        'Should have any other error';
+    $mock_snow->unmock('run_file');
+    $mock_snow->unmock('dbh');
+}
 
 ##############################################################################
 # Can we do live tests?

commit 9ef8d119886785a3c9564c09dac45868888d90e6

Use Snowflake key pair auth, quote identifiers
Switch to key pair authentication in the Snowflake workflows, as
password-only authentication will be discontinued later this year. In
the process, work out and document how to set the appropriate ODBC
parameters in an `odbc.ini` file in preference to the connection URL.

Refactor `snowflake.sh` to∑ set `WORKSPACE` when installing SnowSQL,
which is required to upgrade to v1.3. Use the variable instead of
heard-coded paths throughout, and fix the location of the configuration
file (it has to go into the `.snowsql` subdirectory of the workspace).
Copy and update an `odbc.ini` and `.snowsql/config` as needed for key
pair auth.

Fix the quoting of the role and schema names on connecting to Snowflake,
which were silently failing and therefore failing to properly use the
registry schema, which lead to a failure to find the registry. Broken in
33291bd (#853).

While at it, remove support for the `SNOWSQL_PORT` environment variable,
which has long been deprecated by Snowflake and likely never did
anything. Also add redaction of passwords from Snowflake URL query
parameters in the display URL. Any query parameter matching `pwd` will
now be shown as "REDACTED".

.github/ubuntu/snowflake.sh
@@ -9,27 +9,37 @@ if [ -z "$SKIP_DEPENDS" ]; then
     cat t/odbc/odbcinst.ini | sudo tee -a /etc/odbcinst.ini
 fi
 
+# Set the SnowSQL workspace.
+export WORKSPACE=/opt/snowflake
+
 # https://docs.snowflake.net/manuals/release-notes/client-change-log-snowsql.html
 # https://sfc-repo.snowflakecomputing.com/index.html
-curl -sSLo snowsql.bash https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/1.2/linux_x86_64/snowsql-1.2.21-linux_x86_64.bash
+curl -sSLo snowsql.bash https://sfc-repo.snowflakecomputing.com/snowsql/bootstrap/1.3/linux_x86_64/snowsql-1.3.2-linux_x86_64.bash
 curl -sSLo snowdbc.tgz https://sfc-repo.snowflakecomputing.com/odbc/linux/latest/snowflake_linux_x8664_odbc-3.5.0.tgz
 
 # Install and configure ODBC.
-mkdir -p /opt/snowflake
-sudo tar --strip-components 1 -C /opt/snowflake -xzf snowdbc.tgz
-sudo mv /opt/snowflake/ErrorMessages/en-US /opt/snowflake/lib/
+mkdir -p "$WORKSPACE/.snowsql"
+sudo tar --strip-components 1 -C "$WORKSPACE" -xzf snowdbc.tgz
+sudo mv "$WORKSPACE/ErrorMessages/en-US" "$WORKSPACE/lib/"
+
+# Set up the DSN for key pair auth.
+perl -npE 's/KEY_PASSWORD/$ENV{SNOWFLAKE_KEY_PASSWORD}/g' t/odbc/snowflake.ini | sudo tee -a /etc/odbc.ini
+printf "%s" "${SNOWFLAKE_KEY_FILE}" > "$WORKSPACE/rsa_key.p8"
 
 # Install, update, and configure SnowSQL.
-sed -e '1,/^exit$/d' snowsql.bash | sudo tar -C /opt/snowflake -zxf -
-/opt/snowflake/snowsql -Uv
-printf "[options]\nnoup = true\n" > /opt/snowflake/config
+sed -e '1,/^exit$/d' snowsql.bash | sudo tar -C "$WORKSPACE" -zxf -
+"$WORKSPACE/snowsql" -Uv
+(
+    printf "[connections]\nprivate_key_path=%s/rsa_key.p8\n\n" "$WORKSPACE"
+    printf "[options]\nnoup = true\n"
+) > "$WORKSPACE/.snowsql/config"
 
 # Add to the path.
 if [[ -n "$GITHUB_PATH" ]]; then
-    echo "/opt/snowflake" >> "$GITHUB_PATH"
+    echo "$WORKSPACE" >> "$GITHUB_PATH"
 fi
 
 # Tell SnowSQL where to find the config.
 if [[ -n "$GITHUB_ENV" ]]; then
-    echo "WORKSPACE=/opt/snowflake" >> "$GITHUB_ENV"
+    echo "WORKSPACE=$WORKSPACE" >> "$GITHUB_ENV"
 fi

.github/workflows/coverage.yml
@@ -65,6 +65,8 @@ jobs:
       - name: Setup Clients
         env:
           SKIP_DEPENDS: true
+          SNOWFLAKE_KEY_PASSWORD: ${{ secrets.SNOWFLAKE_KEY_PASSWORD }}
+          SNOWFLAKE_KEY_FILE: ${{ secrets.SNOWFLAKE_KEY_FILE }}
         run: |
           .github/ubuntu/all-apt-prereqs.sh
           .github/ubuntu/exasol.sh
@@ -92,7 +94,8 @@ jobs:
           LIVE_PG_REQUIRED: true
           SQITCH_TEST_PG_URI: db:pg://postgres@/postgres
           LIVE_SNOWFLAKE_REQUIRED: true
-          SQITCH_TEST_SNOWFLAKE_URI: db:snowflake://${{ secrets.SNOWFLAKE_USERNAME }}:${{ secrets.SNOWFLAKE_PASSWORD }}@sra81677.us-east-1/sqitchtest?Driver=Snowflake;warehouse=compute_wh
+          SNOWSQL_PRIVATE_KEY_PASSPHRASE: ${{ secrets.SNOWFLAKE_KEY_PASSWORD }}
+          SQITCH_TEST_SNOWFLAKE_URI: db:snowflake://${{ secrets.SNOWFLAKE_USERNAME }}@sra81677.us-east-1/sqitchtest?DSN=sqitch;warehouse=compute_wh
           LIVE_SQLITE_REQUIRED: true
           LIVE_VERTICA_REQUIRED: true
           SQITCH_TEST_VSQL_URI: db:vertica://dbadmin@localhost:${{ job.services.vertica.ports[5433] }}/VMart?Driver=Vertica

.github/workflows/snowflake.yml
@@ -13,6 +13,9 @@ jobs:
     steps:
       - uses: actions/checkout@v4
       - name: Setup Clients
+        env:
+          SNOWFLAKE_KEY_PASSWORD: ${{ secrets.SNOWFLAKE_KEY_PASSWORD }}
+          SNOWFLAKE_KEY_FILE: ${{ secrets.SNOWFLAKE_KEY_FILE }}
         run: .github/ubuntu/snowflake.sh
       - name: Setup Perl
         id: perl
@@ -29,5 +32,6 @@ jobs:
         env:
           PERL5LIB: "${{ github.workspace }}/local/lib/perl5"
           LIVE_SNOWFLAKE_REQUIRED: true
-          SQITCH_TEST_SNOWFLAKE_URI: db:snowflake://${{ secrets.SNOWFLAKE_USERNAME }}:${{ secrets.SNOWFLAKE_PASSWORD }}@sra81677.us-east-1/sqitchtest?Driver=Snowflake;warehouse=compute_wh
+          SNOWSQL_PRIVATE_KEY_PASSPHRASE: ${{ secrets.SNOWFLAKE_KEY_PASSWORD }}
+          SQITCH_TEST_SNOWFLAKE_URI: db:snowflake://${{ secrets.SNOWFLAKE_USERNAME }}@sra81677.us-east-1/sqitchtest?DSN=sqitch;warehouse=compute_wh
         run: prove -lvr t/snowflake.t

lib/App/Sqitch/Engine/snowflake.pm
@@ -24,9 +24,12 @@ sub destination {
     # Just use the target name if it doesn't look like a URI.
     return $self->target->name if $self->target->name !~ /:/;
 
-    # Use the URI sans password.
+    # Use the URI sans passwords.
     my $uri = $self->target->uri->clone;
     $uri->password(undef) if $uri->password;
+    for my $key (grep { /pwd/ } $uri->query_params) {
+        $uri->query_param($key => 'REDACTED');
+    }
     return $uri->as_string;
 }
 
@@ -98,10 +101,6 @@ has uri => (
 
         # Set defaults in the URI.
         $uri->host($self->_host($uri));
-        # Use _port instead of port so it's empty if no port is in the URI.
-        # https://github.com/sqitchers/sqitch/issues/675
-        # XXX SNOWSQL_PORT deprecated; remove once Snowflake removes it.
-        $uri->port($ENV{SNOWSQL_PORT}) if !$uri->_port && $ENV{SNOWSQL_PORT};
         $uri->dbname(
             $ENV{SNOWSQL_DATABASE}
             || $self->_snowcfg->{dbname}
@@ -204,21 +203,23 @@ has dbh => (
                 connected => sub {
                     my $dbh = shift;
                     my $role = $self->role;
+                    # Use LITERAL(), but not for WAREHOUSE, which might be
+                    # database-qualified (db.wh). Details on IDENTIFIER():
+                    # https://docs.snowflake.com/en/sql-reference/identifier-literal
                     $dbh->do($_) or return for (
-                        ($role ? ("USE ROLE $role") : ()),
+                        ($role ? ('USE ROLE IDENTIFIER(' . $dbh->quote($role) . ')') : ()),
                         "ALTER WAREHOUSE $wh RESUME IF SUSPENDED",
                         "USE WAREHOUSE $wh",
                         'ALTER SESSION SET TIMESTAMP_TYPE_MAPPING=TIMESTAMP_LTZ',
                         "ALTER SESSION SET TIMESTAMP_OUTPUT_FORMAT='YYYY-MM-DD HH24:MI:SS'",
                         "ALTER SESSION SET TIMEZONE='UTC'",
                     );
-                    $dbh->do('USE SCHEMA ' . $dbh->quote_identifier($self->registry))
+                    $dbh->do('USE SCHEMA IDENTIFIER(' . $dbh->quote($self->registry) . ')')
                         or $self->_handle_no_registry($dbh);
                     return;
                 },
                 disconnect => sub {
                     my $dbh = shift;
-                    my $wh = $self->warehouse;
                     $dbh->do("ALTER WAREHOUSE $wh SUSPEND");
                     return;
                 },

lib/sqitch-authentication.pod
@@ -159,16 +159,28 @@ query string, e.g.,
 
 Snowflake does not support password-less authentication, but does support
 key-pair authentication. Follow
-L<the instructions|https://docs.snowflake.com/en/user-guide/snowsql-start.html#using-key-pair-authentication>
-to create a key pair, then set the following variables in the F<~/.snowsql/config>
-file:
+L<the instructions|https://docs.snowflake.com/en/user-guide/key-pair-auth>
+to create a key pair, then set the C<private_key_path> in the F<~/.snowsql/config>
+to point to the private key file:
 
-  authenticator = SNOWFLAKE_JWT
-  private_key_path = "path/to/privatekey.p8"
+  private_key_path = "<path>/rsa_key.p8"
 
 To connect, set the C<$SNOWSQL_PRIVATE_KEY_PASSPHRASE> environment variable to
-the passphrase for the private key, and add these parameters to the query part
-of your connection URI:
+the passphrase for the private key, and add these parameters under the
+configuration for your DSN in F</etc/odbc.ini> or F<~/.odbc.ini>:
+
+  [sqitch]
+  AUTHENTICATOR     = SNOWFLAKE_JWT
+  UID               = <username>
+  PRIV_KEY_FILE     = <path>/rsa_key.p8
+  PRIV_KEY_FILE_PWD = <password>
+
+Then connect using the named DSN via the C<DSN> query parameter:
+
+  db:snowflake://movera@example.snowflakecomputing.com/flipr?warehouse=compute_wh;DSN=sqitch
+
+Or add the ODBC parameters directly to the query part of your connection URI
+(although it's safer to put C<priv_key_file_pwd> in F<odbc.ini>):
 
 =over
 
@@ -185,6 +197,10 @@ of your connection URI:
 For example:
 
   db:snowflake://movera@example.snowflakecomputing.com/flipr?Driver=Snowflake;warehouse=sqitch;authenticator=SNOWFLAKE_JWT;uid=movera;priv_key_file=path/to/privatekey.p8;priv_key_file_pwd=s0up3rs3cre7
+
+Sadly, both the C<SNOWSQL_PRIVATE_KEY_PASSPHRASE> environment variable and
+the C<priv_key_file_pwd> ODBC parameter must be set, as Sqitch uses ODBC to
+maintain its registry and SnowSQL to execute change scripts.
 
 =back
 

lib/sqitch-environment.pod
@@ -316,11 +316,6 @@ parameter.
 The Snowflake server host to connect to. Superseded by the target URI host
 name. Deprecated by Snowflake.
 
-=item C<SNOWSQL_PORT>
-
-The Snowflake server port to connect to. Superseded by the target URI port.
-Deprecated by Snowflake.
-
 =item C<SNOWSQL_REGION>
 
 The Snowflake region. Superseded by the target URI host name. Deprecated by

t/odbc/snowflake.ini
@@ -1,0 +1,9 @@
+[ODBC Data Sources]
+sqitch = Snowflake
+
+[sqitch]
+Driver            = /opt/snowflake/lib/libSnowflake.so
+ACCOUNT           = YOHTWZV-ZSA24461
+AUTHENTICATOR     = SNOWFLAKE_JWT
+PRIV_KEY_FILE     = /opt/snowflake/rsa_key.p8
+PRIV_KEY_FILE_PWD = KEY_PASSWORD

t/snowflake.t
@@ -32,7 +32,7 @@ use TestConfig;
 
 my $CLASS;
 
-delete $ENV{"SNOWSQL_$_"} for qw(USER PASSWORD DATABASE HOST PORT);
+delete $ENV{"SNOWSQL_$_"} for qw(USER PASSWORD DATABASE HOST);
 
 BEGIN {
     $CLASS = 'App::Sqitch::Engine::snowflake';
@@ -87,6 +87,30 @@ is $snow->destination, $exp_uri, 'Destination should be URI string';
 is $snow->registry_destination, $snow->destination,
     'Registry destination should be the same as destination';
 
+# Test destination URI redaction.
+PASSWORDS: {
+    my $sensitive_uri = "db:snowflake://julie:s3cr3t@/?pwd=xyz;key_pwd=abc;pod=x";
+    my $target = App::Sqitch::Target->new(
+        sqitch => $sqitch,
+        uri    => URI::db->new($sensitive_uri),
+    );
+    isa_ok my $sensitive_snow = $CLASS->new(
+        sqitch => $sqitch,
+        target => $target,
+    ), $CLASS;
+
+    # Test URI.
+    my $exp = URI::db->new($sensitive_uri);
+    $exp->host("$ENV{SNOWSQL_ACCOUNT}.snowflakecomputing.com");
+    $exp->dbname('julie');
+    is $sensitive_snow->uri, $exp, 'Should have sensitive info in URI';
+
+    # Test redacted destination URI.
+    $exp->query('pwd=REDACTED;key_pwd=REDACTED;pod=x');
+    $exp->password(undef);
+    is $sensitive_snow->destination, $exp, 'Should have sensitive info in URI';
+}
+
 # Test environment variables.
 SNOWENV: {
     local $ENV{SNOWSQL_USER} = 'kamala';
@@ -95,12 +119,11 @@ SNOWENV: {
     local $ENV{SNOWSQL_WAREHOUSE} = 'madrigal';
     local $ENV{SNOWSQL_ACCOUNT} = 'egregious';
     local $ENV{SNOWSQL_HOST} = 'test.us-east-2.aws.snowflakecomputing.com';
-    local $ENV{SNOWSQL_PORT} = 4242;
     local $ENV{SNOWSQL_DATABASE} = 'tryme';
 
     my $target = App::Sqitch::Target->new(sqitch => $sqitch, uri => URI->new($uri));
     my $snow = $CLASS->new( sqitch => $sqitch, target => $target );
-    is $snow->uri, 'db:snowflake://test.us-east-2.aws.snowflakecomputing.com:4242/tryme',
+    is $snow->uri, 'db:snowflake://test.us-east-2.aws.snowflakecomputing.com/tryme',
         'Should build URI from environment';
     is $snow->username, 'kamala', 'Should read username from environment';
     is $snow->password, 'gimme', 'Should read password from environment';
@@ -112,7 +135,7 @@ SNOWENV: {
     delete $ENV{SNOWSQL_HOST};
     $snow = $CLASS->new( sqitch => $sqitch, target => $target );
     is $snow->uri,
-        'db:snowflake://egregious.Australia.snowflakecomputing.com:4242/tryme',
+        'db:snowflake://egregious.Australia.snowflakecomputing.com/tryme',
         'Should build URI host from account and region environment vars';
     is $snow->account, 'egregious.Australia',
         'Should read account and region from environment';